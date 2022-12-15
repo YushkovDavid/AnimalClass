6a1bdc1e04999aeccc325d16e94845850cb54f55
YushkovDavid <120051423+YushkovDavid@users.noreply.github.com>
2022-12-15 16:52:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from Elephant import *
 from Penguin import *
 from Giraffe import *
+from Valier import *
+
 g1 = Penguin("Виталя", 4, 12)
 g1._foodTypes = "Мать"
 g1.Eat("Мать")
+
+V = Valier()
+V.AddAnimal(g1)
 
 
